6530ab1233aae82dfdb38e8a5415013de5c9872a
qiyanatech <qiyanatech@gmail.com>
2024-08-22 17:10:19 -0300

    add lombok and lombok annotation to all classes

--- src/main/java/org/example/model/LineItem.java
+++ src/main/java/org/example/model/LineItem.java
@@ -1,8 +1,13 @@
 package org.example.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class LineItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,18 +17,13 @@ public class LineItem {
     private double totalPrice;
     private String currency;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne
     @JoinColumn(name = "order_id")
     private Orders orders;
 
     @OneToOne
     @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product product;
-
-
-    public LineItem(){
-
-    }
 
     public LineItem(Integer id, Integer quantity, double totalPrice, String currency, Product product, Orders order) {
         this.id = id;
@@ -33,26 +33,6 @@ public class LineItem {
         this.product = product;
         this.orders = order;
 
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public Product getProduct() {
-        return product;
     }
 
     @Override
--- src/main/java/org/example/model/Orders.java
+++ src/main/java/org/example/model/Orders.java
@@ -1,11 +1,16 @@
 package org.example.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,29 +20,14 @@ public class Orders {
     @JoinColumn(name = "customer_id",nullable = false)
     private Customer customer;
 
-    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "orders", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LineItem> lineItems = new ArrayList<>();
-
-    public Orders(){
-
-    }
 
     public Orders(Integer id,Customer customer) {
         this.customer = customer;
         this.id = id;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public List<LineItem> getLineItems() {
-        return lineItems;
-    }
 
     @Override
     public String toString() {
