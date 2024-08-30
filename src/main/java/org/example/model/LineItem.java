1e283ec8aae1c227c5ca7a41dfdccae8458d7662
QiyanaTech <qiyanatech@gmail.com>
2024-08-30 00:23:30 -0300

    format with google-style

--- src/main/java/org/example/model/LineItem.java
+++ src/main/java/org/example/model/LineItem.java
@@ -1,48 +1,58 @@
 package org.example.model;
 
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Getter
 public class LineItem {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private Integer quantity;
-    private double totalPrice;
-    private String currency;
+  private Integer quantity;
+  private double totalPrice;
+  private String currency;
 
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    private Orders orders;
+  @ManyToOne
+  @JoinColumn(name = "order_id")
+  private Order order;
 
-    @OneToOne
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
-    private Product product;
+  @OneToOne
+  @JoinColumn(name = "product_id", referencedColumnName = "id")
+  private Product product;
 
-    public LineItem(Integer id, Integer quantity, double totalPrice, String currency, Product product, Orders order) {
-        this.id = id;
-        this.quantity = quantity;
-        this.totalPrice = totalPrice;
-        this.currency = currency;
-        this.product = product;
-        this.orders = order;
+  public LineItem(
+      Long id,
+      Integer quantity,
+      double totalPrice,
+      String currency,
+      Product product,
+      Order order) {
+    this.id = id;
+    this.quantity = quantity;
+    this.totalPrice = totalPrice;
+    this.currency = currency;
+    this.product = product;
+    this.order = order;
+  }
 
-    }
-
-    @Override
-    public String toString() {
-        return "LineItem{" +
-                "id=" + id +
-                ", quantity=" + quantity +
-                ", totalPrice=" + totalPrice +
-                ", currency='" + currency + '\'' +
-                ", product=" + product +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "LineItem{"
+        + "id="
+        + id
+        + ", quantity="
+        + quantity
+        + ", totalPrice="
+        + totalPrice
+        + ", currency='"
+        + currency
+        + '\''
+        + ", product="
+        + product
+        + '}';
+  }
 }
