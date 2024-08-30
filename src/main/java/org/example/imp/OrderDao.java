2bc4b12c4203f923bdbd2b9319d7443b02a78d41
QiyanaTech <qiyanatech@gmail.com>
2024-08-30 14:49:59 -0300

    remove specific methods

--- src/main/java/org/example/imp/OrderDao.java
+++ src/main/java/org/example/imp/OrderDao.java
@@ -87,43 +87,6 @@ public class OrderDao implements Dao<Order> {
     return order;
   }
 
-  public Order getByLineItem(Integer order_id) {
-    Order order = null;
-    try {
-      CriteriaQuery<Order> cq = cb.createQuery(Order.class);
-      Root<LineItem> lineItemRoot = cq.from(LineItem.class);
-      Join<LineItem, Order> orderJoin = lineItemRoot.join("order");
-
-      cq.select(orderJoin).where(cb.equal(orderJoin.get("id"), order_id));
-      order = em.createQuery(cq).getSingleResult();
-    } catch (Exception e) {
-      log.error("Unable to find order. {}", e.getMessage());
-    } finally {
-      em.getTransaction().commit();
-    }
-
-    return order;
-  }
-
-  public List<Order> getByCustomer(String name, String address) {
-    TypedQuery<Order> query = null;
-    try {
-      CriteriaQuery<Order> criteriaQuery = cb.createQuery(Order.class);
-      Root<Order> orderRoot = criteriaQuery.from(Order.class);
-
-      Join<Order, Customer> customerJoin = orderRoot.join("customer");
-
-      Predicate namePredicate = cb.equal(customerJoin.get("name"), name);
-
-      Predicate addressPredicate = cb.like(customerJoin.get("address"), "%" + address + "%");
-      criteriaQuery.where(cb.and(namePredicate, addressPredicate));
-      query = em.createQuery(criteriaQuery);
-    } catch (IllegalArgumentException e) {
-      log.error("Unable to find order{}", e.getMessage());
-    }
-    return query.getResultList();
-  }
-
   @Override
   public void delete(Long orderId) {
     try {
--- src/test/java/com/example/OrderDAOTest.java
+++ src/test/java/com/example/OrderDAOTest.java
@@ -3,16 +3,24 @@ package com.example;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
 import org.example.imp.OrderDao;
 import org.example.model.Customer;
 import org.example.model.LineItem;
 import org.example.model.Order;
 import org.example.model.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -20,15 +28,32 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderDAOTest {
 
+  private static final String LOG_PATH = "logs/app.log";
+  static Integer orderId = 1;
+
   EntityManagerFactory etf = Persistence.createEntityManagerFactory("persistence");
   OrderDao orderDao = new OrderDao(etf);
-  static Integer orderId = 1;
+
+  EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
+  EntityManager emMock = mock(EntityManager.class);
+  EntityTransaction transaction = mock(EntityTransaction.class);
+  OrderDao orderDaoMock = new OrderDao(etfMock);
+  CriteriaBuilder cbMock = mock(CriteriaBuilder.class);
 
   Customer customer =
       new Customer(null, "QiyanaTech", "test@gmail.com", "Rua Tiberius Dourado", "123456789");
   Product product = new Product(null, "Hair Spray", "300 ml", 5, "dollars");
   Order order = new Order(null, customer);
   LineItem lineItem = new LineItem(null, 2, 10, "dollars", product, order);
+
+  @BeforeEach
+  public void tearDown() throws IOException {
+    Files.write(
+            Paths.get(LOG_PATH),
+            new byte[0],
+            StandardOpenOption.TRUNCATE_EXISTING,
+            StandardOpenOption.CREATE);
+  }
 
   @Test
   @org.junit.jupiter.api.Order(1)
@@ -38,42 +63,33 @@ public class OrderDAOTest {
   }
 
   @Test
-  void getAllTest_mustReturnAll() {
+  void
+  addOrderTest_whenAddAnOrderFails_mustNotThrowException() {
+
+    when(etfMock.createEntityManager()).thenReturn(emMock);
+    when(emMock.getTransaction()).thenReturn(transaction);
+
+    doThrow(new IllegalArgumentException()).when(emMock).persist(any());
+
+    assertDoesNotThrow(
+        () -> {
+          orderDaoMock.add(order);
+        });
+  }
+
+  @Test
+  @org.junit.jupiter.api.Order(5)
+  void getAllTest_mustReturnAllOrders() {
     List<Order> list = List.of();
     assertNotEquals(list, orderDao.getAll());
   }
 
   @Test
-  void addOrderTest_whenAddAnOrderFails_mustNotThrowException() {
-    EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
-    EntityManager emMock = mock(EntityManager.class);
-    EntityTransaction transaction = mock(EntityTransaction.class);
-    when(etfMock.createEntityManager()).thenReturn(emMock);
-    when(emMock.getTransaction()).thenReturn(transaction);
-
-    doThrow(new IllegalArgumentException()).when(emMock).persist(any());
-    OrderDao orderDAO1 = new OrderDao(etfMock);
-    assertDoesNotThrow(
-        () -> {
-          orderDAO1.add(order);
-        });
-  }
-
-  @Test
-  @org.junit.jupiter.api.Order(3)
-  void getOrderByLineItemTest_whenLineItemIsValid_mustReturnTheOrder() {
-    assertNotNull(orderDao.getByLineItem(orderId));
-  }
-
-  @Test
-  @org.junit.jupiter.api.Order(4)
-  void getOrdersByCustomerTest_whenCustomerAndAddressIsValid_mustReturnAllOrders() {
-    assertNotNull(orderDao.getByCustomer("QiyanaTech", "Tiberius"));
-  }
-
-  @Test
-  @org.junit.jupiter.api.Order(5)
-  void deleteTest_whenOrderIdIsValid_mustNotThrowException() {
-     orderDao.delete(1L);
+  @org.junit.jupiter.api.Order(6)
+  void deleteTest_whenOrderIdIsValid_mustNotThrowException() throws IOException {
+    orderDao.delete(1L);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
+    assertFalse(logContent.contains("Unable to delete order."));
   }
 }
