101a211ffe21c14dffc858f45a2d5478edc27fcc
QiyanaTech <qiyanatech@gmail.com>
2024-08-30 00:23:30 -0300

    format with google-style

--- src/main/java/org/example/model/Product.java
+++ src/main/java/org/example/model/Product.java
@@ -1,40 +1,46 @@
 package org.example.model;
 
-
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Getter
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String name;
-    private String description;
-    private double price;
-    private String currency;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    public Product(Integer id, String name, String description, double price, String currency) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.currency = currency;
+  private String name;
+  private String description;
+  private double price;
+  private String currency;
 
-    }
+  public Product(Long id, String name, String description, double price, String currency) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.currency = currency;
+  }
 
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", currency='" + currency + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Product{"
+        + "id="
+        + id
+        + ", name='"
+        + name
+        + '\''
+        + ", description='"
+        + description
+        + '\''
+        + ", price="
+        + price
+        + ", currency='"
+        + currency
+        + '\''
+        + '}';
+  }
 }
