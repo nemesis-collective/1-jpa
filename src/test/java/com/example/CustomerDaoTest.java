d1af1a565a8c0a63351d12fecf44c97b563443e2
QiyanaTech <jp20171617@gmail.com>
2024-09-12 22:56:03 -0300

    change tests to be independent of each other

--- src/test/java/com/example/CustomerDaoTest.java
+++ src/test/java/com/example/CustomerDaoTest.java
@@ -1,128 +1,145 @@
 package com.example;
 
-import org.example.imp.CustomerDao;
-import org.example.model.Customer;
-import org.junit.jupiter.api.*;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import org.example.DAO.imp.CustomerDao;
+import org.example.model.Customer;
+import org.junit.jupiter.api.*;
+
 public class CustomerDaoTest {
-    private static final String LOG_PATH = "logs/app.log";
+  private static final String LOG_PATH = "logs/app.log";
 
-    EntityManagerFactory etf = Persistence.createEntityManagerFactory("persistence");
-    CustomerDao customerDao = new CustomerDao(etf);
-    Customer customer =
-            new Customer(null, "QiyanaTech", "test@gmail.com", "Rua Tiberius Dourado", "123456789");
+  EntityManagerFactory etf;
+  CustomerDao customerDao;
+  Customer customer;
 
+  @BeforeEach
+  public void tearDown() throws IOException {
+    Files.write(
+        Paths.get(LOG_PATH),
+        new byte[0],
+        StandardOpenOption.TRUNCATE_EXISTING,
+        StandardOpenOption.CREATE);
+    etf = Persistence.createEntityManagerFactory("persistence");
+    customerDao = new CustomerDao(etf);
+    customer =
+        new Customer(null, "QiyanaTech", "test@gmail.com", "Rua Tiberius Dourado", "123456789");
+  }
 
-    @BeforeEach
-    public void tearDown() throws IOException {
-        Files.write(
-                Paths.get(LOG_PATH),
-                new byte[0],
-                StandardOpenOption.TRUNCATE_EXISTING,
-                StandardOpenOption.CREATE);
-    }
+  @Test
+  void addCustomerTest_whenAddCustomer_mustNotThrowException() {
+    assertDoesNotThrow(() -> customerDao.add(customer));
+  }
 
-    @Test
-    void addCustomerTest_whenAddCustomer_mustNotThrowException() {
-        assertDoesNotThrow(() -> customerDao.add(customer));
-    }
+  @Test
+  void addCustomerTest_whenAddCustomerFails_mustNotThrowException() {
+    EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
+    EntityManager emMock = mock(EntityManager.class);
+    EntityTransaction transaction = mock(EntityTransaction.class);
 
-    @Test
-    void addCustomerTest_whenAddCustomerFails_mustNotThrowException() {
-        EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
-        EntityManager emMock = mock(EntityManager.class);
-        EntityTransaction transaction = mock(EntityTransaction.class);
+    when(etfMock.createEntityManager()).thenReturn(emMock);
+    when(emMock.getTransaction()).thenReturn(transaction);
+    doThrow(new IllegalArgumentException()).when(emMock).persist(any());
 
-        when(etfMock.createEntityManager()).thenReturn(emMock);
-        when(emMock.getTransaction()).thenReturn(transaction);
-        doThrow(new IllegalArgumentException()).when(emMock).persist(any());
+    CustomerDao customerDaoMock = new CustomerDao(etfMock);
 
-        CustomerDao customerDaoMock = new CustomerDao(etfMock);
+    assertDoesNotThrow(() -> customerDaoMock.add(customer));
+  }
 
-        assertDoesNotThrow(() -> customerDaoMock.add(customer));
-    }
+  @Test
+  void getTest_whenCustomerIdIsValid_mustReturnCustomer() {
+    customerDao.add(customer);
+    assertNotNull(customerDao.get(4L));
+  }
 
-    @Test
-    void getTest_whenCustomerIdIsValid_mustReturnCustomer() {
-        assertNotNull(customerDao.get(1L));
-    }
+  @Test
+  void getTest_whenCustomerIdIsInvalid_mustNotThrowException() throws IOException {
+    customerDao.get(5L);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
+    assertTrue(logContent.contains("Unable to get customer."));
+  }
 
-    @Test
-    void getTest_whenCustomerIdIsInvalid_mustNotThrowException() throws IOException {
-        customerDao.get(2L);
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
-        assertTrue(logContent.contains("Unable to get customer."));
-    }
+  @Test
+  void getAllTest_mustReturnAllCustomer() throws IOException {
+    customerDao.getAll();
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
 
-    @Test
-    void getAllTest_mustReturnAllCustomer() {
-        assertDoesNotThrow(() -> customerDao.getAll());
-    }
+    assertFalse(logContent.contains("Unable to get all customers."));
+  }
 
-    @Test
-    void getAllTest_whenOccursException_mustNotThrow() throws IOException {
-        EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
-        EntityManager emMock = mock(EntityManager.class);
-        EntityTransaction transaction = mock(EntityTransaction.class);
-        CriteriaBuilder cb = mock(CriteriaBuilder.class);
+  @Test
+  void getAllTest_whenOccursException_mustNotThrow() throws IOException {
+    EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
+    EntityManager emMock = mock(EntityManager.class);
+    EntityTransaction transaction = mock(EntityTransaction.class);
+    CriteriaBuilder cb = mock(CriteriaBuilder.class);
 
-        when(etfMock.createEntityManager()).thenReturn(emMock);
-        when(emMock.getTransaction()).thenReturn(transaction);
-        when(emMock.getCriteriaBuilder()).thenReturn(cb);
-        when(cb.createQuery()).thenThrow(new IllegalArgumentException());
+    when(etfMock.createEntityManager()).thenReturn(emMock);
+    when(emMock.getTransaction()).thenReturn(transaction);
+    when(emMock.getCriteriaBuilder()).thenReturn(cb);
+    when(cb.createQuery()).thenThrow(new IllegalArgumentException());
 
-        CustomerDao customerDaoMock = new CustomerDao(etfMock);
-        customerDaoMock.getAll();
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
+    CustomerDao customerDaoMock = new CustomerDao(etfMock);
+    customerDaoMock.getAll();
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
 
-        assertTrue(logContent.contains("Unable to get all customers."));
-    }
+    assertTrue(logContent.contains("Unable to get all customers."));
+  }
 
-    @Test
-    void updateTest_whenCustomerIsValid_mustNotThrowException() {
-        Customer customer1 = new Customer(1L,"QiyanaTech1","teste456@gmail.com","Rua Ixtal","1254785336");
-        assertDoesNotThrow(() -> customerDao.update(customer1));
-    }
+  @Test
+  void updateTest_whenCustomerIsValid_mustNotThrowException() throws IOException {
+    customerDao.add(customer);
+    Customer customer1 =
+        new Customer(1L, "QiyanaTech1", "teste456@gmail.com", "Rua Ixtal", "1254785336");
+    customerDao.update(customer1);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
 
-    @Test
-    void updateTest_whenCustomerIsInvalid_mustReceiveErrorMessage() throws IOException {
-        Customer customer1 = new Customer(2L,"QiyanaTech","teste456@gmail.com","Rua Ixtal","1254785336");
-        customerDao.update(customer1);
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
-        assertTrue(logContent.contains("Unable to update customer."));
-    }
+    assertFalse(logContent.contains("Unable to update customer."));
+  }
 
-    @Test
-    void deleteTest_whenCustomerIdIsValid_mustNotThrowException(){
-        assertDoesNotThrow(() -> customerDao.delete(1L));
-    }
+  @Test
+  void updateTest_whenCustomerIsInvalid_mustReceiveErrorMessage() throws IOException {
+    Customer customer1 =
+        new Customer(5L, "QiyanaTech", "teste456@gmail.com", "Rua Ixtal", "1254785336");
+    customerDao.update(customer1);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
+    assertTrue(logContent.contains("Unable to update customer."));
+  }
 
-    @Test
-    void deleteTest_whenCustomerIdIsInvalid_mustReceiveErrorMessage() throws IOException {
-        customerDao.delete(2L);
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
+  @Test
+  void deleteTest_whenCustomerIdIsValid_mustNotThrowException() throws IOException {
+    customerDao.add(customer);
+    customerDao.delete(2L);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
 
-        assertTrue(logContent.contains("Unable to delete customer."));
-    }
+    assertFalse(logContent.contains("Unable to delete customer."));
+  }
+
+  @Test
+  void deleteTest_whenCustomerIdIsInvalid_mustReceiveErrorMessage() throws IOException {
+    customerDao.delete(5L);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
+
+    assertTrue(logContent.contains("Unable to delete customer."));
+  }
 }
--- src/test/java/com/example/LineItemDaoTest.java
+++ src/test/java/com/example/LineItemDaoTest.java
@@ -1,155 +1,149 @@
 package com.example;
 
-import org.example.imp.CustomerDao;
-import org.example.imp.LineItemDao;
-import org.example.imp.OrderDao;
-import org.example.imp.ProductDao;
-import org.example.model.Customer;
-import org.example.model.LineItem;
-import org.example.model.Product;
-import org.junit.jupiter.api.*;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import org.example.DAO.imp.CustomerDao;
+import org.example.DAO.imp.LineItemDao;
+import org.example.DAO.imp.OrderDao;
+import org.example.DAO.imp.ProductDao;
+import org.example.model.Customer;
+import org.example.model.LineItem;
+import org.example.model.Product;
+import org.junit.jupiter.api.*;
+
 public class LineItemDaoTest {
-    private static final String LOG_PATH = "logs/app.log";
+  private static final String LOG_PATH = "logs/app.log";
 
-    EntityManagerFactory etf = Persistence.createEntityManagerFactory("persistence");
-    CustomerDao customerDao = new CustomerDao(etf);
-    OrderDao orderDao = new OrderDao(etf);
-    ProductDao productDao = new ProductDao(etf);
-    LineItemDao lineItemDao = new LineItemDao(etf);
-    Customer customer =
-            new Customer(null, "QiyanaTech", "test@gmail.com", "Rua Tiberius Dourado", "123456789");
+  EntityManagerFactory etf = Persistence.createEntityManagerFactory("persistence");
+  CustomerDao customerDao = new CustomerDao(etf);
+  OrderDao orderDao = new OrderDao(etf);
+  ProductDao productDao = new ProductDao(etf);
+  LineItemDao lineItemDao = new LineItemDao(etf);
+  Customer customer =
+      new Customer(null, "QiyanaTech", "test@gmail.com", "Rua Tiberius Dourado", "123456789");
 
-    org.example.model.Order order = new org.example.model.Order(null, customer, "pending", "Pix");
+  org.example.model.Order order = new org.example.model.Order(null, customer, "pending", "Pix");
 
-    Product product = new Product(null, "Hair Spray", "300 ml", 5, "dollars");
+  Product product = new Product(null, "Hair Spray", "300 ml", 5, "dollars");
 
-    LineItem lineItem = new LineItem(null, 2, 10, "dollars", product, order);
+  LineItem lineItem = new LineItem(null, 2, 10, "dollars", product, order);
 
-    @BeforeEach
-    public void tearDown() throws IOException {
-        Files.write(
-                Paths.get(LOG_PATH),
-                new byte[0],
-                StandardOpenOption.TRUNCATE_EXISTING,
-                StandardOpenOption.CREATE);
-    }
+  @BeforeEach
+  public void tearDown() throws IOException {
+    Files.write(
+        Paths.get(LOG_PATH),
+        new byte[0],
+        StandardOpenOption.TRUNCATE_EXISTING,
+        StandardOpenOption.CREATE);
+  }
 
+  @Test
+  void addTest_whenAddItem_mustNotThrowException() {
+    customerDao.add(customer);
+    productDao.add(product);
+    orderDao.add(order);
+    assertDoesNotThrow(() -> lineItemDao.add(lineItem));
+  }
 
-    @Test
-    @Order(1)
-    void addTest_whenAddItem_mustNotThrowException() {
-        customerDao.add(customer);
-        productDao.add(product);
-        orderDao.add(order);
-        assertDoesNotThrow(() -> lineItemDao.add(lineItem));
-    }
+  @Test
+  void addTest_whenAddItemFails_mustReceiveErrorMessage() {
+    EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
+    EntityManager emMock = mock(EntityManager.class);
+    EntityTransaction transaction = mock(EntityTransaction.class);
 
-    @Test
-    @Order(2)
-    void addTest_whenAddItemFails_mustReceiveErrorMessage() {
-        EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
-        EntityManager emMock = mock(EntityManager.class);
-        EntityTransaction transaction = mock(EntityTransaction.class);
+    when(etfMock.createEntityManager()).thenReturn(emMock);
+    when(emMock.getTransaction()).thenReturn(transaction);
+    doThrow(new IllegalArgumentException()).when(emMock).persist(any());
 
-        when(etfMock.createEntityManager()).thenReturn(emMock);
-        when(emMock.getTransaction()).thenReturn(transaction);
-        doThrow(new IllegalArgumentException()).when(emMock).persist(any());
+    LineItemDao lineItemDao1 = new LineItemDao(etfMock);
 
-        LineItemDao lineItemDao1 = new LineItemDao(etfMock);
+    assertDoesNotThrow(() -> lineItemDao1.add(lineItem));
+  }
 
-        assertDoesNotThrow(() -> lineItemDao1.add(lineItem));
-    }
+  @Test
+  void getTest_whenItemIdIsValid_mustReturnItem() {
+    customerDao.add(customer);
+    productDao.add(product);
+    orderDao.add(order);
+    lineItemDao.add(lineItem);
+    assertNotNull(lineItemDao.get(1L));
+  }
 
-    @Test
-    @Order(3)
-    void getTest_whenItemIdIsValid_mustReturnItem() {
-        assertNotNull(lineItemDao.get(1L));
-    }
+  @Test
+  void getTest_whenItemIdIsInvalid_mustNotThrowException() throws IOException {
+    lineItemDao.get(5L);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
+    assertTrue(logContent.contains("Unable to get item."));
+  }
 
-    @Test
-    @Order(4)
-    void getTest_whenItemIdIsInvalid_mustNotThrowException() throws IOException {
-        lineItemDao.get(2L);
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
-        assertTrue(logContent.contains("Unable to get item."));
-    }
+  @Test
+  void getAllTest_mustReturnAllItems() {
+    assertDoesNotThrow(() -> lineItemDao.getAll());
+  }
 
-    @Test
-    @Order(5)
-    void getAllTest_mustReturnAllItems() {
-        assertDoesNotThrow(() -> lineItemDao.getAll());
-    }
+  @Test
+  void getAllTest_whenOccursException_mustReceiveErrorMessage() throws IOException {
+    EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
+    EntityManager emMock = mock(EntityManager.class);
+    EntityTransaction transaction = mock(EntityTransaction.class);
+    CriteriaBuilder cb = mock(CriteriaBuilder.class);
 
-    @Test
-    @Order(6)
-    void getAllTest_whenOccursException_mustReceiveErrorMessage() throws IOException {
-        EntityManagerFactory etfMock = mock(EntityManagerFactory.class);
-        EntityManager emMock = mock(EntityManager.class);
-        EntityTransaction transaction = mock(EntityTransaction.class);
-        CriteriaBuilder cb = mock(CriteriaBuilder.class);
+    when(etfMock.createEntityManager()).thenReturn(emMock);
+    when(emMock.getTransaction()).thenReturn(transaction);
+    when(emMock.getCriteriaBuilder()).thenReturn(cb);
+    when(cb.createQuery()).thenThrow(new IllegalArgumentException());
 
-        when(etfMock.createEntityManager()).thenReturn(emMock);
-        when(emMock.getTransaction()).thenReturn(transaction);
-        when(emMock.getCriteriaBuilder()).thenReturn(cb);
-        when(cb.createQuery()).thenThrow(new IllegalArgumentException());
+    LineItemDao lineItemDao1 = new LineItemDao(etfMock);
+    lineItemDao1.getAll();
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
 
-        LineItemDao lineItemDao1 = new LineItemDao(etfMock);
-        lineItemDao1.getAll();
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
+    assertTrue(logContent.contains("Unable to get all items."));
+  }
 
-        assertTrue(logContent.contains("Unable to get all items."));
-    }
+  @Test
+  void updateTest_whenItemIsValid_mustNotThrowException() {
+    LineItem lineItem1 = new LineItem(1L, 3, 15, "dollars", product, order);
+    assertDoesNotThrow(() -> lineItemDao.update(lineItem1));
+  }
 
-    @Test
-    @Order(7)
-    void updateTest_whenItemIsValid_mustNotThrowException() {
-        LineItem lineItem1 = new LineItem(1L, 3, 15, "dollars", product, order);
-        assertDoesNotThrow(() -> lineItemDao.update(lineItem1));
-    }
+  @Test
+  void updateTest_whenItemIsInvalid_mustReceiveErrorMessage() throws IOException {
+    customerDao.add(customer);
+    productDao.add(product);
+    orderDao.add(order);
+    LineItem lineItem1 = new LineItem(5L, 3, 15, "dollars", product, order);
+    lineItemDao.update(lineItem1);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
+    assertTrue(logContent.contains("Unable to update item."));
+  }
 
-    @Test
-    @Order(8)
-    void updateTest_whenItemIsInvalid_mustReceiveErrorMessage() throws IOException {
-        LineItem lineItem1 = new LineItem(2L, 3, 15, "dollars", product, order);
-        lineItemDao.update(lineItem1);
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
-        assertTrue(logContent.contains("Unable to update item."));
-    }
+  @Test
+  void deleteTest_whenItemIdIsValid_mustNotThrowException() {
+    assertDoesNotThrow(() -> lineItemDao.delete(1L));
+  }
 
-    @Test
-    @Order(9)
-    void deleteTest_whenItemIdIsValid_mustNotThrowException() throws IOException {
-        assertDoesNotThrow(() -> lineItemDao.delete(1L));
-    }
+  @Test
+  void deleteTest_whenItemIdIsInvalid_mustReceiveErrorMessage() throws IOException {
+    lineItemDao.delete(5L);
+    final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
+    final String logContent = String.join("\n", logLines);
 
-    @Test
-    @Order(10)
-    void deleteTest_whenItemIdIsInvalid_mustReceiveErrorMessage() throws IOException {
-        lineItemDao.delete(2L);
-        final List<String> logLines = Files.readAllLines(Paths.get(LOG_PATH));
-        final String logContent = String.join("\n", logLines);
-
-        assertTrue(logContent.contains("Unable to delete item."));
-    }
+    assertTrue(logContent.contains("Unable to delete item."));
+  }
 }
