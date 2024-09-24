60421c3c6911f063d15238c20a625556dd14399b
QiyanaTech <jp20171617@gmail.com>
2024-09-24 16:57:28 -0300

    change currency attribute to use the currency class

--- src/main/java/org/qiyana/model/Product.java
+++ src/main/java/org/qiyana/model/Product.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Currency;
+
 @Entity
 @NoArgsConstructor
 @Getter
@@ -15,14 +17,14 @@ public class Product {
   private String name;
   private String description;
   private double price;
-  private String currency;
+  private Currency currency;
 
-  public Product(Long id, String name, String description, double price, String currency) {
+  public Product(Long id, String name, String description, double price, String currencyCode) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
-    this.currency = currency;
+    this.currency = Currency.getInstance(currencyCode);
   }
 
   @Override
